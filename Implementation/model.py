06429ed26858965eef918e61c7c47356a256329f
Jason <2014145110@yonsei.ac.kr>
2020-08-02 15:21:01 +0900

    Code Implementation - Model.py, ipynb
    Add group convolutions on AlexNet

--- Implementation/model.py
+++ Implementation/model.py
@@ -8,13 +8,13 @@ class alexnet(nn.Module):
     def __init__(self, num_classes=1000):
         super(alexnet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2), nn.ReLU(inplace=True),
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2), nn.BatchNorm2d(96), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(inplace=True),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=2), nn.BatchNorm2d(256), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1, groups=2), nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=2), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
